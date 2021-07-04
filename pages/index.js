bf694fa25035e5b44c0248fca88c7e759b75eb30
kantarow <kanta7306@gmail.com>
2021-07-04 18:34:16 +0900

    Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
@@ -19,7 +21,6 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
@@ -30,9 +31,18 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link
+                href={{
+                  pathname: 'posts/[id]',
+                  query: {id: id},
+                }}
+              >
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
               <br />
             </li>
           ))}
